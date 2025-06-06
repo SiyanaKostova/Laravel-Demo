e87272695d142b10c41e7c21e8db20e5ad29054b
Siyana Kostova <siyana.kostova06@abv.bg>
2025-06-06 09:26:26 +0300

    Job Policy implemented.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
-use App\Models\User;
-use Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -32,31 +32,34 @@ class JobController extends Controller
     {
         request()->validate([
             'title' => ['required', 'min:3'],
-            'salary' => ['required'],
+            'salary' => ['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        // Mail::to($job->employer->user)->queue(
+        //     new JobPosted($job)
+        // );
 
         return redirect('/jobs');
     }
 
     public function edit(Job $job)
     {
-        Gate::authorize('edit-job', $job);
-
         return view('jobs.edit', ['job' => $job]);
     }
 
-
     public function update(Job $job)
     {
+        Gate::authorize('edit-job', $job);
+
         request()->validate([
             'title' => ['required', 'min:3'],
-            'salary' => ['required'],
+            'salary' => ['required']
         ]);
 
         $job->update([
@@ -69,6 +72,8 @@ class JobController extends Controller
 
     public function destroy(Job $job)
     {
+        Gate::authorize('edit-job', $job);
+
         $job->delete();
 
         return redirect('/jobs');
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,7 +8,7 @@
         This job pays {{ $job->salary }} per year.
     </p>
 
-    @can('edit-job', $job)
+    @can('edit', $job)
         <a href="/jobs/{{ $job->id }}/edit"
             class="relative inline-flex items-center px-4 py-2 ml-3 mt-6 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300">
             Edit Job
